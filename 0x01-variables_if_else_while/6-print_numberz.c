2aa7bb62488b3d5ab3daf2055edcda9f8505059e
chibwesamuel <smchibwe@gmail.com>
2022-09-09 13:56:01 -0700

    more numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0 - 9
+ *
+ * Description: Using the main function we list numbers from 0 - 9
+ *
+ * Return: 0
+ */
+
+int main(voi)
+{
+	char x;
+
+	for (x = '0'; x < 9; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
