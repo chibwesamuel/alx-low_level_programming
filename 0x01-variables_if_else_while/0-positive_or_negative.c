e2221ec8271d1fde2aa8a565bb389f59b99bb092
chibwesamuel <smchibwe@gmail.com>
2022-09-09 07:26:48 -0700

    pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/** 
- * main - assign a random number to the variable n each 
- * 	time and prints whether the number stored in the 
- * 	variable n is positive or negative 
- * return: always  0
+/**
+ * main - assign a random number to the variable n each
+ * 	time and prints whether the number stored in the
+ * 	variable n is positive or negative
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -18,7 +19,7 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else 
+	else
 		printf("%d is negative\n", n);
 	return (0);
 }
