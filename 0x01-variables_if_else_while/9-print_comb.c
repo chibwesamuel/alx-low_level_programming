6e02bfca1da6553231895156d72b3bc26e5ba00b
chibwesamuel <smchibwe@gmail.com>
2022-09-09 15:19:23 -0700

    q9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations
+ *
+ * Description: the main function prints all possible
+ * combinatiotions of numbers
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	if (i != 57)
+		putchar(',');
+	return (0);
+}
