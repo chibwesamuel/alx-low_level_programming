e033bda752fd6b25e8d1571feb2e738eeb4acb70
chibwesamuel <smchibwe@gmail.com>
2022-09-09 12:44:33 -0700

    mixed alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print letters of the alphabet in lowercase and uppercase
+ *
+ * Description: using the main function
+ * this program prints letters of the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
