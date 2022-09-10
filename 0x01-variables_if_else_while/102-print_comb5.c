e6405249c5a6e68dc7e9827024dac1c0d36ee65f
chibwesamuel <smchibwe@gmail.com>
2022-09-10 11:35:52 -0700

    final

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+
+/**
+ * main - all possible different combinations of two digits
+ *
+ * Description: through the main function this program
+ * all possible different combinations of two digits
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 100; x++)
+		for (y = 0; y <= 100; y++)
+			if (y > x)
+				putchar((x / 10) + '0');
+	putchar((x % 10) + '0');
+	putchar(' ');
+	putchar((y / 10) + '0');
+	putchar((y % 10) + '0');
+	if (x != 98)
+		putchar(',');
+	putchar('\n');
+	return (0);
+}
