b7d274ef4acbae4f09fba832d91e47600f5b6923
chibwesamuel <smchibwe@gmail.com>
2022-09-09 23:40:09 -0700

    q10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,13 +18,13 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	
 	for (x = 0; x <= 9; x++)
-		if (x > i)
-			putchar(i + '0');
-			putchar(x + 0);
+	if (x > i)
+		putchar(i + '0');
+		putchar(x + 0);
 		
-		if (i != 8)
-			putchar(',');
-			putchar(' ');
+	if (i != 8)
+		putchar(',');
+		putchar(' ');
 	putchar('\n');
 	return (0);
 }
