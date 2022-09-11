08c745c093b00d90894aa68d2572c6f41aa30cf8
chibwesamuel <smchibwe@gmail.com>
2022-09-11 12:37:17 -0700

    Let me go

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,12 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++){
 		putchar(i);
 		if (i <= '8')
 			putchar(',');
 		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
