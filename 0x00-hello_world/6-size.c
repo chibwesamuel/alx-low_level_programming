519ca296cfe6577cb058d79bd48281dc11daa21f
chibwesamuel <smchibwe@gmail.com>
2022-09-12 13:49:13 -0700

    cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ printf("size of an int: %1u byte(s)\n", (unsigned)sizeof(a));
 printf("size of a long int: %1u byte(s)\n", (unsigned)sizeof(b));
 printf("size of a long long int: %1u byte(s)\n", (unsigned)sizeof(c));
 printf("size of a float: %1u byte(s)\n", (unsigned)sizeof(f));
-fprintf(stderr, [Anything]);
+fprintf(stderr, "[Anything]");
 return (0);
 }
