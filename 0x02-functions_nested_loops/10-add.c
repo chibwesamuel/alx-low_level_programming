b69b51375c987ea3dbc7cca7755eaecc2aa69f14
chibwesamuel <smchibwe@gmail.com>
2022-09-14 07:38:48 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add_num - function that adds two numbers
+ *
+ * @one: fist number
+ * @two: second number
+ * Return: 0
+ *
+ */
+
+void add_num(void)
+{
+	int add(int one, int two);
+
+	return (one + two);
+}
