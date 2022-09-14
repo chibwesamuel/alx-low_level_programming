58803d73d1c46287802e1a4cf97427c8b742d916
chibwesamuel <smchibwe@gmail.com>
2022-09-14 14:28:39 -0700

    sure commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,31 +2,28 @@
 #include <stdio.h>
 
 /**
- * print_times_table - Entry point
+ * main - Entry point
  * @n: size of table
  * Return: Always 0 (Success)
  *
  */
 
-void print_times_table(void)
+int main(void)
 {
 	int row, column, tableValue, n;
-
 	n = 10;
-
 	for (column = 1; column <= n; column++)
-		printf_s(" %3d", column);
+		printf(" %3d", column);
 	
 	for (row = 1;row <= n; row++)
-		printf_s(" %2d", row);
+		printf(" %2d", row);
 	
 	for (column = 1; column <= n; column++)
 		tableValue = row * column;
-
 	if (row <= 0 || row >= 15)
 		break;
 	else
-		printf_s(" %3d", tableValue);
+		printf(" %3d", tableValue);
 
 	return (0);
 }
