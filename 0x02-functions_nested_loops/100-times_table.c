4e49edbfaef0726445329718d5ad873d71733636
chibwesamuel <smchibwe@gmail.com>
2022-09-14 14:18:07 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,12 +3,30 @@
 
 /**
  * print_times_table - Entry point
- * @n: input
+ * @n: size of table
  * Return: Always 0 (Success)
  *
  */
 
 void print_times_table(int n)
 {
+	int row, column, tableValue, n;
+
+	n = 10;
+
+	for (column = 1; column <= n; column++)
+		printf_s(" %3d", column);
+	
+	for (row = 1;row <= n; row++)
+		printf_s(" %2d", row);
+	
+	for (column = 1; column <= n; column++)
+		tableValue = row * column;
+
+	if (row <= 0 || row >= 15)
+		break;
+	else
+		printf_s(" %3d", tableValue);
+
 	return (0);
 }
