ad1f5c5fdff559f37cc169136bf31b1763f7c0ca
chibwesamuel <smchibwe@gmail.com>
2022-09-14 03:26:09 -0700

    Jack B

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -23,7 +23,7 @@ void jack_bauer(void)
 			_putchar(hours / 10 + '0');
 			_putchar(hours_remainder + '0');
 			_putchar(':');
-			_putchar(minss_remainder + '0');
+			_putchar(mins_remainder + '0');
 			minutes++;
 			_putchar('\n');
 		}
