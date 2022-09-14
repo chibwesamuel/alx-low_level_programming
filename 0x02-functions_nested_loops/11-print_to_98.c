ef6cdecdd2aa5b3374a7d418b3eb82357488e043
chibwesamuel <smchibwe@gmail.com>
2022-09-14 08:19:17 -0700

    q10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * print_to_98 - function prints natural numbers from n to 98
+ * separated by a comma
+ * @n: the initial number
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+	       printf("%d\n", n);	
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
