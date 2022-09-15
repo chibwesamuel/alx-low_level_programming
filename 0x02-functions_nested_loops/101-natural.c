0292fb5133d0d08c22db3658bc6c14f5af9d5b0d
chibwesamuel <smchibwe@gmail.com>
2022-09-15 02:06:24 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - list all the natural numbers below 10 that are
+ * multiples of 3 or 5, we get 3, 5, 6 and 9
+ *
+ * Return: always 0
+ *
+ */
+
+int main(voi)
+{
+	total_sum = 0
+	
+	for i in range(1, 1000):
+		if (i % 3 == 0 or i % 5 == 0):
+			total_sum = total_sum + i
+		
+		printf(total_sum);
+}
