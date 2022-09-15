bd5ba4eb18b5cd7bf33ac151433ebe841fa5941c
chibwesamuel <smchibwe@gmail.com>
2022-09-15 13:14:33 -0700

    diag

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,29 +4,29 @@
 /**
  * print_diagonal - prints a diagonal
  * @n: parameter
- * Return: returns nothing
+ * Return: always returns 0
  */
 
 void print_diagonal(int n)
 {
-	int len, space;
-
-	if (n > 0)
+	int co, sp;
+	
+	if (n <= 0)
 	{
-		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
-			}
-			putchar('\');
-
-			if (len == (n - 1))
-			{
-				continue;
-			}
-			putchar('\n');
-		}
+		putchar('\n');
 	}
-putchar('\n');
+	else
+	{
+		for (co = 1; co <= n; co++)
+			{
+				for (sp = 1; sp < co; sp++)
+				{
+					putchar(' ');
+				}
+				
+				putchar('\\');
+				
+				putchar('\n');
+			}
+	}
 }
