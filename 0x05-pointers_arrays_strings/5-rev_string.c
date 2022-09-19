82b3906c94ad290e023824b1f5fc1ea867d5c0d2
chibwesamuel <smchibwe@gmail.com>
2022-09-19 11:47:26 -0700

    Revisit Q5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,8 +10,19 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s);
+	int ini = 0, finish = 0;
+	char t;
 
-	while (len--)
-		putchar(*s);
+	while (s[finish] != 0)
+		finish++;
+	finish--;
+
+	while (ini < finish)
+	{
+		t = s[ini];
+		s[ini] = s[finish];
+		s[finish] = t;
+		ini++;
+		finish--;
+	}
 }
