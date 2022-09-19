aa970795e97f20ce91287b7965da7012e9bdefd9
chibwesamuel <smchibwe@gmail.com>
2022-09-19 12:43:56 -0700

    final

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,21 @@
 
 int _atoi(char *s)
 {
-	int i = _atoi(s);
+	int i, j, n, m;
 
-	putchar(i);
-	return (0);
+	i = n = 0;
+	m = 1;
+	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	{
+		if (*(s + i) == '-')
+			m *= -1;
+		i++;
+	}
+	j = i;
+	while ((*(s + j) >= '0') && (*(s + j) <= '9'))
+	{
+		n = n * 10 + m * (*(s + j) - '0');
+		j++;
+	}
+	return (n);
 }
