30cb9cedd4c45eef85151b33723c2053c470dee0
chibwesamuel <smchibwe@gmail.com>
2022-09-19 08:15:56 -0700

    Rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that reverses the content of an array of int
+ * @a: content
+ * @n: elements of content as it be
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
