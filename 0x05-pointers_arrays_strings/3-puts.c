1daa31de2afbacbe523cd64d5ef45c91a7904be9
chibwesamuel <smchibwe@gmail.com>
2022-09-19 07:36:29 -0700

    Str 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * _puts - prints a string
+ * @str: string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + 1) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar('\n');
+}
