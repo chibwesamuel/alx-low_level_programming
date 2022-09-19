1d1327effc0db84bcb8cf8aee9823cbd69d1a71c
chibwesamuel <smchibwe@gmail.com>
2022-09-19 08:25:53 -0700

    Refix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,15 @@
 
 void print_rev(char *s)
 {
-	int len = strlen(s);
+	int i = 0;
 
-	while (len--)
-		putchar(*(s + len));
-	putchar(10);
+	while (s[i] != 0)
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
 }
