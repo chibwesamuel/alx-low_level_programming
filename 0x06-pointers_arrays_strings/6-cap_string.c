7dfc591a0b792670222992baa0939f0c355b3d34
chibwesamuel <smchibwe@gmail.com>
2022-09-22 00:27:52 -0700

    Cap & separaters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @s: string to be turned to uppercase
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+	
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
+		i++;
+	}
+	return (s);
+}
