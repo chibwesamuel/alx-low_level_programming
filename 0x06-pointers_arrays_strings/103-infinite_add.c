b32cae48a9e1b6a3892f485fca543ba8f9d500a8
chibwesamuel <smchibwe@gmail.com>
2022-09-22 02:29:18 -0700

    infinite

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: number 1
+ * @n2: number 2
+ * @r: return value
+ *
+ * Return: result of addition
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, n;
+	i = j = n = 0;
+	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
+	{
+		r[0] = 1 + '0';
+		j = 1;
+	}
+	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
+	{
+		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10
+			n = 1;
+		else
+			n = 0;
+		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
+		r[j] = r[j] % 10 + '0';
+		i++;
+		j++;
+		if (n1[i] == '\0' || n2[i] == '\0')
+			r[j] = '\0';
+	}
+	r[j] = '\0';
+	return (0);
+}
