cc48212f09ec3e02fb7686aedb195d204e6884b1
chibwesamuel <smchibwe@gmail.com>
2022-09-22 01:40:10 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: pointer to input string
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = r[j];
+				boolean = 1;
+			}
+		}
+	}
+	return (s);
+}
