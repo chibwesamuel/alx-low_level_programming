da7fc94ce04b1f35ee903534b0110d4d6d4d22ef
chibwesamuel <smchibwe@gmail.com>
2022-09-28 11:15:43 -0700

    Final touches

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * check - checks if a string is a palindrome
+ * @s: string to be checked
+ *
+ * Return: 1 if true and 0 if false
+ */
+
+void check(char *s, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (s[start] == s[end])
+			*flag *= 1;
+		else
+			*flag *= 0;
+		check(s, start + 1, end - 1, flag);
+	}
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: string to be copied
+ *
+ * Return: 1 if true and 0 if false
+ */
+
+int is_palindrome(char *s)
+{
+	int flag = 1;
+
+	check (s, 0, _strlen_recursion(s) - 1, &flag);
+	return (flag);
+}
