ae3881569f451096b3fa27448fa4f7254ac800bf
chibwesamuel <smchibwe@gmail.com>
2022-09-28 08:01:37 -0700

    Work en Sweat!

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ *
+ * @s: string to be measured
+ *
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return(sum);
+}
