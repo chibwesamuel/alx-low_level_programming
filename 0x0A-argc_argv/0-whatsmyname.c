72a918c7d926bc31b637502f52de72d113cc41a5
chibwesamuel <smchibwe@gmail.com>
2022-09-30 10:48:00 -0700

    Fixed Q0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,17 +4,17 @@
 
 /**
  * main - prints its name, followed by a new line
- * 
+ *
  * @argc: number of arguments supplied
  * @argv: pointers to strings
- * 
+ *
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-   (void) argc;
-   printf(" %s\n", argv[0]);
+	(void) argc;
+	printf(" %s\n", argv[0]);
 
-   return 0;
+	return (0);
 }
