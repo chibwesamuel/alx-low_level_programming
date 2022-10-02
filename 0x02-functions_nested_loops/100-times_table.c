5f10cabaf31c22d26c738e5f27af6cbdd01c6d59
chibwesamuel <smchibwe@gmail.com>
2022-10-02 04:58:51 -0700

    100 TT

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,27 +8,46 @@
  *
  */
 
-int print_times_table(void)
+void print_times_table(int n)
 {
-	int row, column, tableValue, n;
-	n = 10;
-	for (column = 1; column <= n; column++)
-		printf(" %3d", column);
-	_putchar('\n');
-	
-	for (row = 1;row <= n; row++)
-		printf(" %2d", row);
-	_putchar('\n');
-	
-	for (column = 1; column <= n; column++)
-		tableValue = row * column;
-	_putchar('\n');
-	/*
-	 * if (row <= 0 || row >= 15)
-	 * _putchar('null');
-	 * else
-	 */
-	printf(" %3d", tableValue);
+	int x = 0, y, z;
 
-	return (0);
+	if (n > 15 || n < 0)
+		return;
+
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
