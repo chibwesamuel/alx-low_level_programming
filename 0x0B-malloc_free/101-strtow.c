76abe434a3d291b1a42994f5545a8460a4073b9b
chibwesamuel <smchibwe@gmail.com>
2022-10-04 07:30:45 -0700

    Strtow1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,89 +13,32 @@ int count_words(char *str);
 
 char **strtow(char *str)
 {
-	int index = 0;
-	int len = 0;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	while (*(str + index) && *(str + index) != ' ')
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		len++;
-		index++;
+		if (str[i] != 32)
+			con++;
 	}
 
-	return (len);
-}
+	d = malloc(sizeof(char) * con);
 
-/**
- * count_words - Counts the number of words contained within a string.
- * @str: The string to be searched.
- *
- * Return: The number of words in str.
- */
-int count_words(char *str)
-{
-	int index = 0, words = 0, len = 0;
+	if (d == NULL)
+		return (NULL);
 
-	for (index = 0; *(str + index); index++)
-		len++;
-
-	for (index = 0; index < len; index++)
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (*(str + index) != ' ')
+		if (str[i] != 32)
 		{
-			words++;
-			index += word_len(str + index);
+			*d[j] = str[i];
+			j++;
 		}
 	}
-
-	return (words);
-}
-
-/**
- * strtow - Splits a string into words.
- * @str: string to be split in two
- *
- * Return: a pointer to an array or NULL
- */
-char **strtow(char *str)
-{
-	char **strings;
-	int index = 0, words, w, letters, l;
-
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-
-	words = count_words(str);
-	if (words == 0)
-		return (NULL);
-
-	strings = malloc(sizeof(char *) * (words + 1));
-	if (strings == NULL)
-		return (NULL);
-
-	for (w = 0; w < words; w++)
-	{
-		while (str[index] == ' ')
-			index++;
-
-		letters = word_len(str + index);
-
-		strings[w] = malloc(sizeof(char) * (letters + 1));
-
-		if (strings[w] == NULL)
-		{
-			for (; w >= 0; w--)
-				free(strings[w]);
-
-			free(strings);
-			return (NULL);
-		}
-
-		for (l = 0; l < letters; l++)
-			strings[w][l] = str[index++];
-
-		strings[w][l] = '\0';
-	}
-	strings[w] = NULL;
-
-	return (strings);
+	return (d);
 }
