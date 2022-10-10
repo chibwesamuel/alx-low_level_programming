65d67f04dabb2bcf40b132aa6137d83bc58f7972
chibwesamuel <smchibwe@gmail.com>
2022-10-10 02:46:14 -0700

    Added dog.h, main.h & README.d

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - structure that defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This stucture defines a dog and links
+ * this data with owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *name;
+}
