e22393aba92e3423ac61b8073d67d4660a50fbf2
chibwesamuel <smchibwe@gmail.com>
2022-10-21 10:11:49 -0700

    fixed return

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -41,7 +41,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		last = *head;
 		while (last->next != NULL)
 			last = last->next;
-		last->next = next;
+		last->next = new;
 	}
 	return (*head);
 }
