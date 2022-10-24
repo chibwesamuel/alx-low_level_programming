9886ee5a683321ec38f3a588f41683e2a30b0f12
chibwesamuel <smchibwe@gmail.com>
2022-10-24 10:18:40 -0700

    Tasks 0 - 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - function that returns the number of elements in a
+ * linked listint_t list
+ * @h: address to list of elements
+ *
+ * Return: list of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
