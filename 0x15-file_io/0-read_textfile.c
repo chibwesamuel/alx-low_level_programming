e3504bb597d3b1642a19c75ba7e26438e9f20d45
chibwesamuel <smchibwe@gmail.com>
2022-10-31 09:38:24 -0700

    Task 0 first attemt

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the
+ *		POSIX standard output
+ * @filename: pointer to file name
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+}
