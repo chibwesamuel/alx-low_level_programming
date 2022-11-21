9d081e4b9078583392e12fac6b0a2dcc5dda3e42
chibwesamuel <smchibwe@gmail.com>
2022-11-20 23:26:19 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * 
- * This is the entry point of our program
+ * main - prints a given sentence followed by a new line
+ *	to standard output
  *
- * */
+ * Return: 1 on success
+ */
 
 int main(void)
 {
-	printf('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
-	return (0);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n', 59);
+	return (1);
 }
