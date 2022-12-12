75259dfc8371267aebc4bd50b8a312b255bafebd
chibwesamuel <smchibwe@gmail.com>
2022-12-12 05:30:50 -0800

    Added starting docs

---
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to print whether the value of c
+ * is a letter regardless of case
+ *
+ * @c: int that will be used for the argument
+ *
+ * Return: 0
+ *
+ */
+
+int _isalpha(int c)
