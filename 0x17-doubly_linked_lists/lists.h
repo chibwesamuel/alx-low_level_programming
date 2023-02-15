0ea74b57ca3b4f017bf89446a099112b0bdd91c8
chibwesamuel <smchibwe@gmail.com>
2023-02-14 22:22:02 -0800

    More header fix

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -34,5 +34,6 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void* malloc(size_t size);
+void free(void* ptr);
 
 #endif /*LISTS_H*/
