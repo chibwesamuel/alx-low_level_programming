af43e5e96493144da1d3be697b2b92fe877a8e7b
chibwesamuel <smchibwe@gmail.com>
2023-04-11 02:30:37 -0700

    Added Header File

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,37 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/**
+ * main - header file for protoypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+#endif /* SEARCH_ALGOS_H */
